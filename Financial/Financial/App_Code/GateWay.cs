bc3393258bab703475be949831590b0ef573cf20
時浩翔\a0963 <a0963574522@gmail.com>
2022-11-27 09:23:10 +0800

    上傳

---
+++ Financial/Financial/App_Code/GateWay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// GateWay 的摘要描述
+/// </summary>
+public class GateWay
+{
+    public class Response
+    {
+        public string Code = "999";
+        public string Message = "系統發生錯誤";
+        public object datas = null;
+    }
+}
---
+++ Financial/Financial/Public/comparison.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Public_comparison : System.Web.UI.Page
+{
+    public string Stocknumber = string.Empty;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Stocknumber = Request.QueryString["stocknumber"];
+    }
+}
